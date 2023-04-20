4cc37f1ea271cc62a72647fccca486fd869ef7de
Mikko Terho <mikko.terho@brightstraining.com>
2023-04-20 11:42:55 +0300

    kurssitiedot

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Header = (props) => {
+
+  return(
+    <div>
+      <h1>{props.course}</h1>
     </div>
-  );
+  )
+  
+}
+
+const Part = ({p, t}) => {
+  console.log('tämän saa Part', p, t)
+
+  return(
+    <div>
+      <p>{p} - {t}</p>
+    </div>
+  )
+}
+
+// objekti tarvii propseihin ({}) sulkeet
+const Content = ({parts}) => {
+  console.log(parts)
+  
+  return(
+    <div>
+        <Part p = {parts[0].name} t = {parts[0].exercises} /> 
+        <Part p = {parts[1].name} t = {parts[1].exercises}/> 
+        <Part p = {parts[2].name} t = {parts[2].exercises}/>
+    </div>
+  )
+
+}
+
+const Total = (props) => {
+
+  return(
+    <div>
+      <p>Total: {props.total}</p>
+    </div>
+  )
+}
+ 
+
+const App = () => {
+  const course = 'Half Stack application development'
+
+  const parts = [
+    {name:'Fundamentals of React',
+   exercises: 10},
+
+  {name: 'Using props to pass data',
+  exercises:  7},
+
+  {name: 'State of a component',
+  exercises: 13}
+  ]
+  
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
+  
+return (
+  <div>
+    <Header course= {course}/>
+    <Content parts= {parts} />
+    <Total total = {total}/>
+  </div>
+)
+
 }
 
 export default App;
