d4d03928737945bd78cb98b5453c3b98ea9e6cb5
tammenterho <terho.mikko@gmail.com>
2023-04-27 13:48:28 +0300

    render

--- osa3/puhelinluetteloBE/index.js
+++ osa3/puhelinluetteloBE/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json())
 
@@ -100,7 +103,7 @@ app.get('/api/persons/:id', (request, response) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
