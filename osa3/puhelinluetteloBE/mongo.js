5328fe3b9a4122e2b3c559162da012c1496a4d90
tammenterho <terho.mikko@gmail.com>
2023-04-28 15:20:02 +0300

    komentorivi mongodb

---
+++ osa3/puhelinluetteloBE/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url =
+  `mongodb+srv://terho:<password>@test-cluster-1.yn0ozwg.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: name,
+  number: number
+});
+
+Person.find({}).then(result => {
+  result.forEach(person => {
+    console.log(person)
+  })
+  mongoose.connection.close()
+})
+
+person.save().then(result => {
+  console.log('person saved!')
+  mongoose.connection.close()
+})
