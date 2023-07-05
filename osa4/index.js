24a89a167af024dd37c5a75d5b49af01ae2e45ed
tammenterho <terho.mikko@gmail.com>
2023-07-05 15:17:52 +0300

    env, logger and config added

--- osa4/index.js
+++ osa4/index.js
@@ -1,10 +1,13 @@
 const http = require('http')
 const express = require('express')
 const app = express()
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 const cors = require('cors')
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
+//kuvaa tietokannan rakennetta
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
@@ -14,15 +17,11 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const username = 'terho';
-const clusterName = 'blog';
-const dbName = 'test';
-
 const mongoUrl = `mongodb+srv://terho:<password>@blog.bk2kiwh.mongodb.net/test?retryWrites=true&w=majority`
 mongoose.connect(mongoUrl)
 
-app.use(cors())
-app.use(express.json())
+app.use(cors()) // sallii ristikkäisen alkuperän pyynnöt
+app.use(express.json()) //osaa käsitellä json pyyntöjä
 
 app.get('/api/blogs', (request, response) => {
   Blog
@@ -42,7 +41,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
