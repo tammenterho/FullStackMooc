eac68775bd6fa5f388afcf00417fdf38b07fa843
tammenterho <terho.mikko@gmail.com>
2023-07-06 12:54:12 +0300

    4.4 more testing

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
     return 1
   }
   
+  const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => {
+      return sum + blog.likes;
+    }
+    
+    , 0);
+  };
+
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
+
+  
