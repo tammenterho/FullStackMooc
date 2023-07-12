062c7a68561c2cdc5a7e926e5e63cc7426449c27
tammenterho <terho.mikko@gmail.com>
2023-07-12 11:56:21 +0300

    blogs shows and creator of blog

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,14 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
 
 
 
+/*
 blogsRouter.get('/', (request, response) => {
   Blog.find({}).then(blogs => {
     response.json(blogs)
   })
 })
+*/
 
 blogsRouter.get('/', async (request, response) => {
   const notes = await Blog
