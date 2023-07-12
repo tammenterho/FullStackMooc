b56f79762ad4460445bfa941f1d319a0eedc8f3c
tammenterho <terho.mikko@gmail.com>
2023-07-12 17:28:35 +0300

    full works, can start

--- osa5/bloglist-backend/app.js
+++ osa5/bloglist-backend/app.js
@@ -1,36 +1,30 @@
+const http = require('http')
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
 const loginRouter = require('./controllers/login')
-
-mongoose.set('strictQuery', false)
-
-logger.info('connecting to', config.MONGODB_URI)
+const logger = require('./utils/logger')
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info('Connection ok!')
   })
   .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
+    logger.error('ERROR connecting', error.message)
   })
 
 app.use(cors())
-app.use(express.static('build'))
 app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
-app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
 
 module.exports = app
