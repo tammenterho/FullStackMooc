1af9238cd3714fa08c4d364eb8112f6b250cfe7a
tammenterho <terho.mikko@gmail.com>
2023-07-25 19:05:40 +0300

    5.17 start

--- osa5/bloglist-backend/app.js
+++ osa5/bloglist-backend/app.js
@@ -22,6 +22,11 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.tokenExtractor)
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
