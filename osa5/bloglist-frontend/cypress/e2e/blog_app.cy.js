d69d94d0d205b48993cb6a7ac90817bd8b31ae36
tammenterho <terho.mikko@gmail.com>
2023-07-27 00:11:57 +0300

    others than create blog

--- osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -17,29 +17,56 @@ describe('Blog app', function() {
   describe('Log in form', function() {
     beforeEach(function() {
       cy.contains('button', 'log in').click();
+      cy.get('[name="Username"]').type('mikkomikko');
+      cy.get('[name="Password"]').type('salasana');
+      cy.get('button[type="submit"]').click();
     })
 
     it('should have username field', function() {
-      cy.wait(500); // Odota 500 ms
       cy.get('[name="Username"]').should('be.visible');
     });
 
     it('should have password field', function() {
-      cy.wait(500); // Odota 500 ms
       cy.get('[name="Password"]').should('be.visible');
     });
 
     it('should be able to log in successfully', function() {
-      // Täytä käyttäjänimi ja salasana
-      cy.get('[name="Username"]').type('mikkomikko');
-      cy.get('[name="Password"]').type('salasana');
-
-      // Klikkaa "Kirjaudu sisään" -painiketta
-      cy.get('button[type="submit"]').click();
-
-      // Voit tarkistaa, että olet kirjautunut sisään esimerkiksi tervetuloviestin tai jonkin muun elementin avulla
       cy.contains('Blogs').should('be.visible');
       cy.contains('Create New').should('be.visible');
+    });
+
+    it('A blog can be created', function() {
+      cy.get('[name="Title"]').type('Opi koodia kanssani')
+      cy.get('[name="Author"]').type('Josefiina Pythonnaama')
+      cy.get('[name="url"]').type('http://www.opijossunkaa.com')
+
+      // Kirjaa sisään käyttäjä ja hanki JWT-tunniste
+      cy.request('POST', 'http://localhost:3001/api/login', {
+        username: 'mikkomikko',
+        password: 'salasana'
+      }).then(response => {
+        const token = response.body.token;
+
+        // Lisää JWT-tunniste pyynnön otsikkoihin
+        cy.request({
+          method: 'POST',
+          url: 'http://localhost:3001/api/blogs',
+          headers: {
+            'Authorization': `Bearer ${token}`
+          },
+          body: {
+            title: 'Opi koodia kanssani',
+            author: 'Josefiina Pythonnaama',
+            url: 'http://www.opijossunkaa.com'
+          }
+        });
+      });
+
+      // Klikkaa "create" (tallenna) -nappia
+      cy.contains('create').click();
+
+      // Tarkista, että luotu blogi näkyy sivulla
+      cy.contains('Opi koodia kanssani').should('be.visible');
     });
   });
 });
