ad285c31443bb2ad586f5ff290cce4402e1eda84
tammenterho <terho.mikko@gmail.com>
2023-07-26 22:10:10 +0300

    cypress works

--- osa5/bloglist-backend/controllers/testing.js
+++ osa5/bloglist-backend/controllers/testing.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+// tämä on cypress testaamiseen. Resetointi
 router.post('/reset', async (request, response) => {
   await Blog.deleteMany({})
   await User.deleteMany({})
