28807151d59b6ff6c35178105ffd8f0d023e9a00
tammenterho <terho.mikko@gmail.com>
2023-07-26 22:22:46 +0300

    log in button clicked with cypress

--- osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -2,9 +2,26 @@ describe('Blog app', function() {
   beforeEach(function() {
     // cy.request('POST', 'http://localhost:3001/api/testing/reset')
     cy.visit('http://localhost:3000')
+    const user = {
+      name: 'Test user',
+      username: 'tester',
+      password: 'secret'
+    }
   })
 
-  it('Login form is shown', function() {
-    // ...
-  })
-})
+  it('Login button is shown', function() {
+    // Tarkista, että "Log in" -nappi on näkyvissä ja sisältää tekstin "log in"
+    cy.contains('button', 'log in').should('be.visible');
+  });
+
+  it('Can log in successfully', function() {
+    // Klikkaa "Log in" -painiketta
+    cy.contains('button', 'log in').click();
+
+    // Voit tarkistaa esimerkiksi, että olet kirjautunut sisään
+    // Tämä voi olla esimerkiksi tietty viesti tai elementti, joka ilmoittaa onnistuneesta kirjautumisesta.
+    // Esimerkiksi:
+    // cy.contains('Welcome, tester!').should('be.visible');
+    // Tämä olettaa, että kirjautumisen jälkeen näkymässä näytetään tervetuloviesti kirjautuneelle käyttäjälle.
+  });
+});
