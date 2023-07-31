9d2344194580d83af826f4f64c06292b1adb55b9
tammenterho <terho.mikko@gmail.com>
2023-07-31 18:47:11 +0300

    all in own components

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,32 +1,13 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { voteAction } from '../src/reducers/anecdoteReducer';
 import AnecdoteForm from '../src/components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList';
 
 const App = () => {
-  const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
-  const dispatch = useDispatch();
-
-  const vote = (id) => {
-    console.log('vote', id);
-    dispatch(voteAction(id));
-  };
-
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <AnecdoteForm/>
+      <AnecdoteList />
+      <AnecdoteForm />
     </div>
   )
 }
