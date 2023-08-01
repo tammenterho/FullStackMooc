0278b966f80757808733873d699d20d52d59b2e2
tammenterho <terho.mikko@gmail.com>
2023-08-01 16:32:47 +0300

    started filter

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -8,23 +8,23 @@ const AnecdoteList = () => {
     const vote = (id) => {
         console.log('vote', id);
         dispatch(voteAction(id));
-      };
+    };
 
 
     return (
         <div>
-        {anecdotes.map(anecdote =>
-            <div key={anecdote.id}>
-              <div>
-                {anecdote.content}
-              </div>
-              <div>
-                has {anecdote.votes}
-                <button onClick={() => vote(anecdote.id)}>vote</button>
-              </div>
-            </div>
-          )}
-          </div>
+            {anecdotes.map(anecdote =>
+                <div key={anecdote.id}>
+                    <div>
+                        {anecdote.content}
+                    </div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => vote(anecdote.id)}>vote</button>
+                    </div>
+                </div>
+            )}
+        </div>
     )
 }
 
---
+++ osa6/redux-anecdotes/src/components/Filter.js
@@ -0,0 +1,16 @@
+const Filter = () => {
+    const handleChange = (event) => {
+      // input-kentän arvo muuttujassa event.target.value
+    }
+    const style = {
+      marginBottom: 10
+    }
+  
+    return (
+      <div style={style}>
+        filter <input onChange={handleChange} />
+      </div>
+    )
+  }
+  
+  export default Filter
