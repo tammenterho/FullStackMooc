bdc43dc7cca6d66f58065a8650d5971e5da4c262
tammenterho <terho.mikko@gmail.com>
2023-08-04 14:01:11 +0300

    6.20

--- osa6/query-anecdotes/src/App.js
+++ osa6/query-anecdotes/src/App.js
@@ -1,5 +1,7 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery } from 'react-query'
+import axios from 'axios'
 
 const App = () => {
 
@@ -7,6 +9,7 @@ const App = () => {
     console.log('vote')
   }
 
+  /*
   const anecdotes = [
     {
       "content": "If it hurts, do it more often",
@@ -14,6 +17,23 @@ const App = () => {
       "votes": 0
     },
   ]
+*/
+
+  const result = useQuery(
+    'anecdotes',
+    () => axios.get('http://localhost:3001/anecdotes').then(res => res.data)
+  )
+  console.log(result)
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  if ( result.isError) {
+    return <div>Errori tuli</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
